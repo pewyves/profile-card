b7384d8e3a701d4f055a12166e413b934ed4c0fe
Titirat Khunsap <113292765+pewyves@users.noreply.github.com>
2023-07-06 14:06:29 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
 
+function App(){
+  return(
+    <>
+      <div className='card'>
+        <Avatar />
+        <div className='data'>
+          <Intro />
+          <SkillList />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function SkillList(){
+  return(
+    <div className='skill-list'>
+      <Skill skill='feed cat' emoji='😺' color='orange' />
+      <Skill skill='raeder' emoji='🕶' color='skyblue' />
+      <Skill skill='dancer' emoji=' 💃🏻' color='yellow' />
+      <Skill skill='english' emoji='👩🏼‍🦱' color='lime' /> 
+    </div>
+  );
+}
+
+function Skill(props){
+  return(
+    <div className='skill' style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro(){
+  return(
+    <div>
+      <h1>นางสาวธิติรัตน์ ขึ้นทรัพย์</h1>
+      <p>
+        นักศึกษาคณะวิทยาศาสตร์ สาขาการสื่อสารและเทคโนโลยี เป็นคนนอนเยอะ  ชอบฟังเพลงมากๆ สามารถฟังเพลงได้ตลอดเวลา ถ้าวันไหนไม่ได้ฟังเพลงจะรู้สึกว่าวันนั้นขาดอะไรไป ชอบของอร่อยๆ ชอบอ่านฟิคใน ao3 cat people ในอนาคตอยากป็นคนว่างงานที่ร่ำรวย
+      </p>
+    </div>
+  );
+}
+
+function Avatar(){
+  return <img className='avatar' src='MyProfile.jpg' alt="My Avatar" ></img>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
